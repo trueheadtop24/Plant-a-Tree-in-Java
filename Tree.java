29f761a8bb6211b02e75da3a105dd1359553438c
trueheadtop24 <REGINALD.TRUE@SNHU.EDU>
2025-06-03 10:10:45 -0700

    Add files via upload

---
+++ Tree.java
@@ -0,0 +1,18 @@
+// Define your tree class in this file!
+public class Tree{ public static void main(String[] args) {
+  //This code is going to introduce ourselves and print a tree to the screen
+  System.out.println("Hi, I'm Reggie and I'm learning Java!");
+  System.out.println("I am going to plant a tree today!");
+    System.out.println("  *  ");
+    System.out.println(" **** ");
+    System.out.println("****** ");
+    System.out.println("  ** ");
+    System.out.println("  ** ");  
+    System.out.println("* ** *"); 
+    System.out.println("******"); 
+      System.out.println(" **** "); 
+      System.out.println("  **"); 
+      System.out.println("  **"); 
+}
+
+}
